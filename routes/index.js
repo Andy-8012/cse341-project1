8212957541abcf8bbc3c67d12d46498a89b3a0f8
Andy-8012 <andrewvriley@gmail.com>
2026-01-21 22:40:12 -0700

    finished project 1 part 2 completed swagger documentation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
-    res.send("Hello");
+    //swagger.tags-['Hello World']
+    res.send("Hello World");
 });
 
-router.use('/mongo', require('./contacts'))
+router.use('/contacts', require('./contacts'))
 
 module.exports = router;
